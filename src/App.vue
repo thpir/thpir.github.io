6294bb9fd3e2dfe86f3c50ed645d574c3862ee94
thpir <Thijs.Pirmez@howest.be>
2023-07-12 19:13:10 +0200

    deployment

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import { RouterView } from 'vue-router'
+  import Navbar from './components/Navbar.vue'
+  import Background from './components/Background.vue'
+  import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <main>
+    <Navbar/>
+    <Background/>
+    <RouterView/>
+    <Footer/>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    background-color: #1a1a36;
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,85 @@
+<script setup>
+  import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="navbar">
+    <img src="../../public/images/logo.png" alt="THPIR logo" title="THPIR logo" height="50" />
+    <div class="navbar-links">
+      <RouterLink active-class="active" to="/"><div class="link"><img src="../../public/images/icon_home_white_24dp.svg"/><p>Home</p></div></RouterLink>
+      <RouterLink active-class="active" to="/about"><div class="link"><img src="../../public/images/icon_person_white_24dp.svg"/><p>about</p></div></RouterLink>
+      <RouterLink active-class="active" to="/education"><div class="link"><img src="../../public/images/icon_school_white_24dp.svg"/><p>Education</p></div></RouterLink>
+      <RouterLink active-class="active" to="/projects"><div class="link"><img src="../../public/images/icon_workspace_premium_white_24dp.svg"/><p>Projects</p></div></RouterLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  a { 
+    color: #F4F4F4;
+    text-decoration: none; 
+  }
+
+  p {
+    padding: 10px;
+    font-weight: bold;
+    text-align: center;
+    vertical-align: middle;
+  }
+
+  .navbar {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    top: 0px;
+    width: 100vw;
+    padding: 5px 5px 5px 5px;
+    background: linear-gradient(to top, rgba(4, 4, 10, .5), rgba(4, 4, 10, 1), rgba(4, 4, 10, 1)); /* Define a vertical gradient from fully opaque (1) to fully transparent (0) */
+    z-index: 2;
+  }
+
+  .navbar-links {
+    margin-left: auto;
+    margin-right: 0px;
+    float: right;
+    text-decoration: none;
+    display: flex;
+  }
+
+  .link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 5px;
+    margin-right: 5px;
+  }
+
+  a:hover {
+    border-top: 1px solid #197DE6;
+    border-bottom: 1px solid #197DE6;
+    cursor: pointer;
+  }
+
+  a.active {
+    border-top: 1px solid #A82EE4;
+    border-bottom: 1px solid #A82EE4;
+  }
+
+  /* Media query for screens smaller than 600px */
+  @media (max-width: 800px) {
+    p {
+      display: none;
+    }
+
+    a:hover {
+    border-top: none;
+    border-bottom: none;
+    cursor: pointer;
+    }
+
+    a.active {
+      border-top: none;
+      border-bottom: none;
+    }
+  }
+</style>
